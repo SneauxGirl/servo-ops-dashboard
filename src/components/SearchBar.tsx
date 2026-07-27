d8e5cc47588f0dc5023fd0c1d84175edf0cd607c
SneauxGirl <heatherh@liyudev.com>
2026-07-26 22:38:49 -0700

    Add @mui/icons, Search Bar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,32 @@
+import { Search } from "@mui/icons-material";
+import { InputAdornment, TextField } from "@mui/material";
+
+interface SearchBarProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export function SearchBar({
+  value,
+  onChange,
+}: SearchBarProps) {
+    return (
+        <TextField 
+            fullWidth 
+            label="Search Work Orders" 
+            placeholder="Search by part name, part number or id"
+            value={value} 
+            onChange={(e) => onChange(e.target.value)} 
+            slotProps={{
+                input: { 
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <Search />
+                        </InputAdornment>
+                    ),
+                },
+            }}
+        />
+    );
+}
+
--- src/pages/WorkOrders.tsx
+++ src/pages/WorkOrders.tsx
@@ -1,11 +1,31 @@
 import { Container, Stack, Typography} from "@mui/material";
+import { useMemo, useState } from "react";
 import { useAppSelector } from "../app/hooks";
+import { SearchBar } from "../components/SearchBar";
 import { WorkOrderList } from "../features/workOrders/components/WorkOrdersList";
 
 export function WorkOrders() {
     const workOrders = useAppSelector(
         (state) => state.workOrders.items,
     );
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredWorkOrders = useMemo(() => {
+        const query = searchTerm.trim().toLowerCase();
+
+        if (!query) {
+            return workOrders;
+        }
+
+        return workOrders.filter((workOrder) => {
+            return(
+                workOrder.id.toLowerCase().includes(query) ||
+                workOrder.partName.toLowerCase().includes(query) ||
+                workOrder.partNumber.toLowerCase().includes(query));
+        });
+    }, [searchTerm, workOrders]);
+        
 
     return (
         <Container component="main" maxWidth="lg" sx={{ py: 4 }}>
@@ -16,11 +36,13 @@ export function WorkOrders() {
           </Typography>
 
           <Typography color="text.secondary">
-            {workOrders.length} work orders
+            {filteredWorkOrders.length} work orders
           </Typography>
         </div>
 
-        <WorkOrderList workOrders={workOrders} />
+        <SearchBar value={searchTerm} onChange={setSearchTerm}/>
+
+        <WorkOrderList workOrders={filteredWorkOrders} />
       </Stack>
     </Container>
     );
